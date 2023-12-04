5aec3f84dafbda94c69d9b51752f72acc7de09e6
space-barry <khanmuhammadaizazullah@gmail.com>
2023-12-04 05:02:18 +0500

    main

--- src/Player/audios.js
+++ src/Player/audios.js
@@ -1,7 +1,8 @@
 exports.songsdata = [
     {
         "title": "Drake - Forever",
-        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+//        "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+        "url": "http://jamen.do/t/2132711"
     },
     {
         "title": "Linking Park - In the end",
